0dc632bcb97b0b4e81dd971321b9561e14fd3220
nikolasmelui <slak@samaradom.ru>
2020-05-23 16:41:41 +0400

    Add Product structure and unmarshal data on it

--- cmd/ecommerce_faker_client/main.go
+++ cmd/ecommerce_faker_client/main.go
@@ -4,17 +4,52 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-// StrapiRes ...
-// type StrapiRes struct {
-// 	Code    int             `json:"code"`
-// 	Message string          `json:"message"`
-// 	Data    json.RawMessage `json:"data"`
-// }
+// Product ...
+type Product struct {
+	ID                int               `json:"id"`
+	Name              string            `json:"name"`
+	Description       string            `json:"description"`
+	Price             int               `json:"price"`
+	CreatedAt         string            `json:"created_at"`
+	UpdatedAt         string            `json:"updated_at"`
+	ProductCategory   int               `json:"product_category"`
+	ProductProperties []ProductProperty `json:"property"`
+	ProductImages     []ProductImage    `json:"images"`
+	ProductOrders     []ProductOrder    `json:"orders"`
+	ProductCarts      []ProductCart     `json:"carts"`
+	ProductLables     []ProductLable    `json:"lables"`
+}
+
+// ProductProperty ...
+type ProductProperty struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// ProductImage ...
+type ProductImage struct {
+	ID int `json:"id"`
+}
+
+// ProductOrder ...
+type ProductOrder struct {
+	ID int `json:"id"`
+}
+
+// ProductCart ...
+type ProductCart struct {
+	ID int `json:"id"`
+}
+
+// ProductLable ...
+type ProductLable struct {
+	ID int `json:"id"`
+}
 
 type errorResponse struct {
 	Code    int    `json:"code"`
@@ -22,7 +57,7 @@ type errorResponse struct {
 }
 
 func main() {
-	res, err := http.Get("http://localhost:1337/productss")
+	res, err := http.Get("http://localhost:1337/products")
 
 	if err != nil {
 		log.Fatal(err)
@@ -40,20 +75,14 @@ func main() {
 		log.Fatal(newError)
 	}
 
-	buffer, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
+	var products []Product
+	if err = json.NewDecoder(res.Body).Decode(&products); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print(string(buffer))
+	for _, product := range products {
+		fmt.Println("Product ID - ", product.ID)
+		fmt.Println("Product Name - ", product.Name)
+	}
 
-	// var result StrapiResult
-	// if err = json.NewDecoder(res.Body).Decode(&result); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// var result StrapiRes
-
-	// fmt.Print(&result)
 }
