8165795a26b5ed812638f610b646279b793e0031
nikolasmelui <slak@samaradom.ru>
2020-05-23 16:00:56 +0400

    Add response status check with errorResponse struct

--- cmd/ecommerce_faker_client/main.go
+++ cmd/ecommerce_faker_client/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,8 +16,13 @@ import (
 // 	Data    json.RawMessage `json:"data"`
 // }
 
+type errorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
 func main() {
-	res, err := http.Get("http://localhost:1337/products")
+	res, err := http.Get("http://localhost:1337/productss")
 
 	if err != nil {
 		log.Fatal(err)
@@ -23,29 +30,30 @@ func main() {
 
 	defer res.Body.Close()
 
-	if res.StatusCode == http.StatusOK {
-
-		buffer, err := ioutil.ReadAll(res.Body)
-
-		if err != nil {
-			log.Fatal(err)
+	if res.StatusCode != http.StatusOK {
+		var errRes errorResponse
+		if err := json.NewDecoder(res.Body).Decode(&errRes); err == nil {
+			nilError := errors.New(errRes.Message)
+			log.Fatal(nilError)
 		}
-
-		// var result json.RawMessage
-		// var result string
-
-		// if err = json.NewDecoder(res.Body).Decode(&result); err != nil {
-		// 	log.Fatal(err)
-		// }
-
-		fmt.Print(string(buffer))
+		newError := fmt.Errorf("Unknown error, status code: %d", res.StatusCode)
+		log.Fatal(newError)
 	}
 
-	// var result StrapiRes
+	buffer, err := ioutil.ReadAll(res.Body)
 
-	// if err = json.NewDecoder(res.Body).Decode(&result); err == nil {
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(buffer))
+
+	// var result StrapiResult
+	// if err = json.NewDecoder(res.Body).Decode(&result); err != nil {
 	// 	log.Fatal(err)
 	// }
+
+	// var result StrapiRes
 
 	// fmt.Print(&result)
 }
