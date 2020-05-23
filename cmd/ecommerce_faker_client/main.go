bfdb73f5879c0f36ad9af2d4490a40b73395878a
nikolasmelui <slak@samaradom.ru>
2020-05-24 00:18:37 +0400

    Make main.go more flexible

--- cmd/ecommerce_faker_client/main.go
+++ cmd/ecommerce_faker_client/main.go
@@ -6,7 +6,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
+
+// BaseURLV1 ...
+var BaseURLV1 = "http://localhost:1337"
+
+// Client ...
+type Client struct {
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
+// Products ...
+type Products = []Product
 
 // Product ...
 type Product struct {
@@ -57,9 +70,9 @@ type errorResponse struct {
 }
 
 func main() {
+	c := NewClient()
 
-	products, err := getProducts()
-
+	products, err := c.getProducts()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,29 +83,58 @@ func main() {
 	}
 }
 
-func getProducts() (*[]Product, error) {
+// NewClient ...
+func NewClient() *Client {
+	return &Client{
+		BaseURL: BaseURLV1,
+		HTTPClient: &http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
 
-	res, err := http.Get("http://localhost:1337/products")
+func (c *Client) sendRequest(req *http.Request, v interface{}) error {
 
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	res, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	defer res.Body.Close()
 
-	if res.StatusCode != http.StatusOK {
+	if res.StatusCode < http.StatusOK {
 		var errRes errorResponse
 		if err := json.NewDecoder(res.Body).Decode(&errRes); err == nil {
-			return nil, errors.New(errRes.Message)
+			return errors.New(errRes.Message)
 		}
-		return nil, fmt.Errorf("Unknown error, status code: %d", res.StatusCode)
+		return fmt.Errorf("Unknown error, status code: %d", res.StatusCode)
 	}
 
-	var products []Product
-	if err = json.NewDecoder(res.Body).Decode(&products); err != nil {
-		log.Fatal(err)
+	result := v
+
+	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+		return err
 	}
 
-	return &products, nil
+	return nil
+}
+
+func (c *Client) getProducts() (*Products, error) {
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/products", c.BaseURL), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res Products
+
+	if err := c.sendRequest(req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 
 }
