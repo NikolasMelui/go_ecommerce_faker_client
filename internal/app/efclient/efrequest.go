b69b4abb6e444c7f4d8ea4f91178dc22302cf33b
nikolasmelui <slak@samaradom.ru>
2020-05-24 23:18:26 +0400

    Add efrequest file with main request logic from the efclient file

---
+++ internal/app/efclient/efrequest.go
@@ -0,0 +1,43 @@
+package efclient
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type errorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// SendRequest ...
+func (c *Client) SendRequest(req *http.Request, v interface{}) error {
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		var errRes errorResponse
+		if err := json.NewDecoder(res.Body).Decode(&errRes); err == nil {
+			return errors.New(errRes.Message)
+		}
+		return fmt.Errorf("Unknown error, status code: %d", res.StatusCode)
+	}
+
+	result := v
+
+	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+		return err
+	}
+
+	return nil
+}
