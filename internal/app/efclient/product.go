81b5d4401a6f55b33eb7800a70b98dadac024a6e
nikolasmelui <slak@samaradom.ru>
2020-05-25 01:01:46 +0400

    Add first post request CreateProduct

--- internal/app/efclient/product.go
+++ internal/app/efclient/product.go
@@ -1,6 +1,7 @@
 package efclient
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -11,18 +12,18 @@ type Products = []Product
 
 // Product ...
 type Product struct {
-	ID                int             `json:"id"`
-	Name              string          `json:"name"`
-	Description       string          `json:"description"`
-	Price             int             `json:"price"`
-	CreatedAt         string          `json:"created_at"`
-	UpdatedAt         string          `json:"updated_at"`
-	ProductCategory   json.RawMessage `json:"product_category"`
-	ProductProperties []Property      `json:"property"`
-	ProductImages     []Image         `json:"images"`
-	ProductOrders     []Order         `json:"orders"`
-	ProductCarts      []Cart          `json:"carts"`
-	ProductLables     []Label         `json:"lables"`
+	ID              int             `json:"id"`
+	Name            string          `json:"name"`
+	Description     string          `json:"description"`
+	Price           int             `json:"price"`
+	Orders          []Order         `json:"orders"`
+	Carts           []Cart          `json:"carts"`
+	Properties      []Property      `json:"property"`
+	Lables          []Label         `json:"lables"`
+	Images          []Image         `json:"images"`
+	ProductCategory json.RawMessage `json:"product_category"`
+	CreatedAt       string          `json:"created_at"`
+	UpdatedAt       string          `json:"updated_at"`
 }
 
 // Property ...
@@ -61,6 +62,36 @@ func (c *Client) GetProducts() (*Products, error) {
 	}
 
 	var res Products
+
+	if err := c.SendRequest(req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+// CreateProduct ...
+func (c *Client) CreateProduct() (*Product, error) {
+
+	requestBody, err := json.Marshal(map[string]string{
+		"name":             "FirstPostedProduct",
+		"description":      "Description of the first posted product",
+		"price":            "1000",
+		"product_category": "1",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(requestBody)
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/products", c.BaseURL), bytes.NewBuffer(requestBody))
+	// req, err := http.NewRequest("POST", fmt.Sprintf("%s/products", c.BaseURL), bytes.NewBuffer(r))
+	if err != nil {
+		return nil, err
+	}
+
+	var res Product
 
 	if err := c.SendRequest(req, &res); err != nil {
 		return nil, err
