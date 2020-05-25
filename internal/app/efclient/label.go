233d983cb3721a9d5a3a0332ee4100b59860c2dd
nikolasmelui <slak@samaradom.ru>
2020-05-26 01:35:49 +0400

    Big refactoring: Add label module

---
+++ internal/app/efclient/label.go
@@ -0,0 +1,45 @@
+package efclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Label ...
+type Label struct {
+	ID int `json:"id"`
+}
+
+// LabelData ...
+type LabelData struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+// CreateLabel ...
+func (c *Client) CreateLabel(labelData *LabelData) (*Label, error) {
+
+	requestData := map[string]interface{}{
+		"name":        &labelData.Name,
+		"description": &labelData.Description,
+	}
+
+	requestBody, err := json.Marshal(requestData)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/product-labels", c.BaseURL), bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, err
+	}
+
+	var res Label
+	if err := c.SendRequest(req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
