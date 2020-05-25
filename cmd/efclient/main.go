c9ae784df73de1b0b694ddce7ea18b6d0921cef3
nikolasmelui <slak@samaradom.ru>
2020-05-26 01:39:05 +0400

    Cleanup main.go file

--- cmd/efclient/main.go
+++ cmd/efclient/main.go
@@ -182,34 +182,3 @@ func main() {
 	}
 	productWG.Wait()
 }
-
-// Get products
-// chProducts := make(chan *efclient.Products)
-// go func() {
-// 	products, err := c.GetProducts()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	chProducts <- products
-// }()
-// products := <-chProducts
-// for _, product := range *products {
-// 	log.Println("Product ID - ", product.ID)
-// 	log.Println("Product Name - ", product.Name)
-// 	log.Println("ProductCategory ID - ", string(product.ProductCategory))
-// }
-
-// Get product-categories
-// chProductCategories := make(chan *efclient.ProductCategories)
-// go func() {
-// 	productCategories, err := c.GetProductCategories()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	chProductCategories <- productCategories
-// }()
-// productCategories := <-chProductCategories
-// for _, productCategory := range *productCategories {
-// 	log.Println("ProductCategory ID - ", productCategory.ID)
-// 	log.Println("ProductCategory Name - ", productCategory.Name)
-// }
