07be396f7bcae0cb91bfc92256f3f59b797f9d02
nikolasmelui <slak@samaradom.ru>
2020-05-29 02:28:29 +0400

    Add CreateFakeLabels method

--- internal/app/efclient/label.go
+++ internal/app/efclient/label.go
@@ -4,7 +4,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
+	"time"
+
+	"syreclabs.com/go/faker"
+	"syreclabs.com/go/faker/locales"
 )
 
 // Label ...
@@ -42,4 +48,34 @@ func (c *Client) CreateLabel(labelData *LabelData) (*Label, error) {
 	}
 
 	return &res, nil
+}
+
+// CreateFakeLabels ...
+func (c *Client) CreateFakeLabels(wg *sync.WaitGroup, count int) int {
+	faker.Locale = locales.Ru
+	ch := make(chan int, count)
+	ch <- 0
+	for i := 0; i < count; i++ {
+		wg.Add(1)
+		time.Sleep(time.Millisecond * 50)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			fakeLabel := LabelData{
+				Title:       faker.Commerce().Color() + " " + faker.Name().LastName(),
+				Description: faker.Lorem().Sentence(10),
+			}
+			log.Println(fakeLabel)
+			_, err := c.CreateLabel(&fakeLabel)
+			if err != nil {
+				log.Print(fmt.Errorf("%v", err))
+				// log.Fatal(err)
+			} else {
+				counter := <-ch
+				ch <- counter + 1
+			}
+		}(wg)
+	}
+	wg.Wait()
+	close(ch)
+	return <-ch
 }
