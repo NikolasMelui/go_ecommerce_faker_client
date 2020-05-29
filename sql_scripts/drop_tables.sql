1b06850f2ea56e4bd288d300a22f64f43d0f7e71
nikolasmelui <slak@samaradom.ru>
2020-05-30 01:30:56 +0400

    Add dropping counterparty_documents to the drop_tables sql script

--- sql_scripts/drop_tables.sql
+++ sql_scripts/drop_tables.sql
@@ -1,1 +1,9 @@
-drop table if exists products, product_labels, product_categories, "users-permissions_user", orders, orders_documents, counterparties, carts cascade;
+drop table if exists products,
+product_labels,
+product_categories,
+"users-permissions_user",
+orders,
+orders_documents,
+orders_products,
+counterparties,
+counterparty_documents cascade;
