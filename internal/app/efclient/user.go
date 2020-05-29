d7ebc35f06ee136d9f5e6386e05261520f40ffb2
nikolasmelui <slak@samaradom.ru>
2020-05-30 01:29:35 +0400

    Refactor user.go file

--- internal/app/efclient/user.go
+++ internal/app/efclient/user.go
@@ -38,29 +38,24 @@ type UserData struct {
 
 // CreateUser ...
 func (c *Client) CreateUser(userData *UserData) (*User, error) {
-
 	requestData := map[string]interface{}{
 		"username": &userData.Username,
 		"email":    &userData.Email,
 		"password": &userData.Password,
 		"phone":    &userData.Phone,
 	}
-
 	requestBody, err := json.Marshal(requestData)
 	if err != nil {
 		return nil, err
 	}
-
 	req, err := http.NewRequest("POST", fmt.Sprintf("%s/auth/local/register", c.BaseURL), bytes.NewBuffer(requestBody))
 	if err != nil {
 		return nil, err
 	}
-
 	var res User
 	if err := c.SendRequest(req, &res); err != nil {
 		return nil, err
 	}
-
 	return &res, nil
 }
 
