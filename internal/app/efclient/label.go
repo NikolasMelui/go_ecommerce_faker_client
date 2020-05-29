9b2fb78ed5ebd88bd559b3cb6c72d027d5a6f17c
nikolasmelui <slak@samaradom.ru>
2020-05-30 01:29:19 +0400

    Refactor label.go file

--- internal/app/efclient/label.go
+++ internal/app/efclient/label.go
@@ -26,27 +26,22 @@ type LabelData struct {
 
 // CreateLabel ...
 func (c *Client) CreateLabel(labelData *LabelData) (*Label, error) {
-
 	requestData := map[string]interface{}{
 		"title":       &labelData.Title,
 		"description": &labelData.Description,
 	}
-
 	requestBody, err := json.Marshal(requestData)
 	if err != nil {
 		return nil, err
 	}
-
 	req, err := http.NewRequest("POST", fmt.Sprintf("%s/product-labels", c.BaseURL), bytes.NewBuffer(requestBody))
 	if err != nil {
 		return nil, err
 	}
-
 	var res Label
 	if err := c.SendRequest(req, &res); err != nil {
 		return nil, err
 	}
-
 	return &res, nil
 }
 
